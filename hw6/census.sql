79510cc24d1312c28292bb0e203742000a7f905c
Edward Kim <edward.junhyung.kim@gmail.com>
2014-07-19 16:52:23 -0500

    Update census.sql template.

--- hw6/census.sql
+++ hw6/census.sql
@@ -1,5 +1,6 @@
 -- Week 6 Problem 1 Template
 
+DROP TABLE IF EXISTS myCensus;
 CREATE TABLE myCensus -- your code goes here
 
 .separator ,
@@ -8,6 +9,7 @@ CREATE TABLE myCensus -- your code goes here
 -- uncomment below to check the first 10 lines
 -- SELECT * FROM myCensus LIMIT 10;
 
+DROP TABLE IF EXISTS moreCensus;
 CREATE TABLE moreCensus -- your code goes here
 
 .separator ,
@@ -16,6 +18,7 @@ CREATE TABLE moreCensus -- your code goes here
 -- uncomment below to check the first 10 lines
 -- SELECT * FROM moreCensus LIMIT 10;
 
+DROP TABLE IF EXISTS myMoreCensus;
 CREATE TABLE myMoreCensus AS SELECT -- your code goes here
 
 -- uncomment below to check the first 10 lines
